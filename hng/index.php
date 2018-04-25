790d69dbfa2f5a3d4490d3c794b94cef28ca3346
EkenennaR <ekenennar@gmail.com>
2018-04-25 13:15:28 +0100

    My first GIT experience

---
+++ hng/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ekenenna Ugwumba</title>
+</head>
+<style>
+
+    body {
+    	background-color: #FDF7F7;
+    }
+	h1{
+		font-size: 50px; 
+		text-align: center;
+		color: #828282;
+	}
+
+	h3 {
+		font-size: 30px;
+		text-align: center;
+		color: #000;
+	} 
+
+	.title {
+		margin-top: 20%;
+	}
+</style>
+<body>
+	<div class= "title">
+		<h1>EKENENNA UGWUMBA</h1>
+    <h3><?php
+date_default_timezone_set("Africa/Lagos");
+echo "TIME : " . date("h:i:sa");
+?> </h3>
+    
+
+	</div>
+    
+</body>
+</html>
